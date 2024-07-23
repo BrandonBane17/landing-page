198bbbc575c97452010345690d3fc8d258af3746
BrandonBane17 <brandon.bane17@gmail.com>
2024-07-23 16:40:20 -0400

    trying email stuff

---
+++ send_email.php
@@ -0,0 +1,38 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Recipient email
+    $to = "brandon.bane17@gmail.com";
+
+    // Email subject
+    $email_subject = "New message from $name: $subject";
+
+    // Email body
+    $email_body = "You have received a new message from your website contact form.\n\n".
+                  "Here are the details:\n".
+                  "Name: $name\n".
+                  "Email: $email\n".
+                  "Subject: $subject\n".
+                  "Message:\n$message";
+
+    // Email headers
+    $headers = "From: $email\n";
+    $headers .= "Reply-To: $email";
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again.";
+    }
+}
+?>
